dec831117dc260e440c7781cc98f0a80c27fea05
Lukáš Vlček <61832vlcek@student.sstebrno.cz>
2026-03-09 14:12:54 +0100

    Add files via upload

---
+++ 09.03.2026/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09._03._2026
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public int Length { get; set; }
+
+        public double Imdb { get; set; }
+        public double Csfd { get; set; }
+        public int Rotten { get; set; }
+
+        public string Director { get; set; }
+        public string Description { get; set; }
+    }
+}
